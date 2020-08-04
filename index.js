0b0b7aea066456550ed7e479b5488f35e73ae892
Sourabh Kumar Ravi <66831910+SourabhRavi@users.noreply.github.com>
2020-08-04 16:13:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
-var randomNumber1 = Math.floor((Math.random()*6))+1;
-var randomNumber2 = Math.floor((Math.random()*6))+1;
 
-document.querySelector(".img1").setAttribute("src","images/dice" + randomNumber1 + ".png");
-document.querySelector(".img2").setAttribute("src","images/dice" + randomNumber2 + ".png");
+function myFunction() {
 
-if(randomNumber1 > randomNumber2)
-{
-  document.querySelector("h1").innerText = "🚩 Player " + 1 + " wins!";
-  document.querySelector("p").innerText = "Click refresh to play again.";
+  var randomNumber1 = Math.floor((Math.random()*6))+1;
+  var randomNumber2 = Math.floor((Math.random()*6))+1;
+
+
+  document.querySelector(".img1").setAttribute("src","images/dice" + randomNumber1 + ".png");
+  document.querySelector(".img2").setAttribute("src","images/dice" + randomNumber2 + ".png");
+
+  if(randomNumber1 > randomNumber2)
+  {
+    document.querySelector("h1").innerText = "🚩 Player " + 1 + " wins!";
+  }
+
+  else if (randomNumber1 < randomNumber2)
+  {
+    document.querySelector("h1").innerText = "Player " + 2 + " wins! 🚩";
+  }
+
+  else
+  {
+    document.querySelector("h1").innerText = "🚩 Its a draw! 🚩";
+  }
+
+  document.querySelector(".btn").innerText = "Click to play again";
+
 }
 
-else if (randomNumber1 < randomNumber2)
-{
-  document.querySelector("h1").innerText = "Player " + 2 + " wins! 🚩";
-  document.querySelector("p").innerText = "Click refresh to play again.";
+window.onload = function() {
+    var reloading = sessionStorage.getItem("reloading");
+    if (reloading) {
+        sessionStorage.removeItem("reloading");
+        myFunction();
+    }
 }
 
-else
-{
-  document.querySelector("h1").innerText = "🚩 Its a draw! 🚩";
-  document.querySelector("p").innerText = "Click refresh to play again.";
+function reloadPage() {
+    sessionStorage.setItem("reloading", "true");
+    document.location.reload();
 }
