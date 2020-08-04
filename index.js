1dbc6fa4404ee371fb783298002706fab523c646
Sourabh Kumar Ravi <66831910+SourabhRavi@users.noreply.github.com>
2020-08-04 15:10:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,17 @@ document.querySelector(".img2").setAttribute("src","images/dice" + randomNumber2
 if(randomNumber1 > randomNumber2)
 {
   document.querySelector("h1").innerText = "🚩 Player " + 1 + " wins!";
+  document.querySelector("p").innerText = "Click refresh to play again.";
 }
 
 else if (randomNumber1 < randomNumber2)
 {
   document.querySelector("h1").innerText = "Player " + 2 + " wins! 🚩";
+  document.querySelector("p").innerText = "Click refresh to play again.";
 }
 
 else
 {
   document.querySelector("h1").innerText = "🚩 Its a draw! 🚩";
+  document.querySelector("p").innerText = "Click refresh to play again.";
 }
